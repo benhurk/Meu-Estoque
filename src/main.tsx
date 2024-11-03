51533ac6f33d18c404ea7afacab9e8b701902d8b
Ben Hur <benhurk2801@gmail.com>
2024-11-03 15:22:19 -0300

    Implemented redux-persist to store the list state

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store/index.ts'
-import App from './App.tsx'
+import store, { persistor } from './store';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import listReducer from "./reducers/list";
 import modalReducer from "./reducers/modal";
 
+const persistedListReducer = persistReducer({ key: 'root', storage }, listReducer);
+
 const store = configureStore({
     reducer: {
-        list: listReducer,
+        list: persistedListReducer,
         modal: modalReducer
-    }
-})
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE]
+        }
+    })
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
