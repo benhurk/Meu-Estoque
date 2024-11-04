072e4693386842f7cbab6699c4a1fef12cb970b9
Ben Hur <benhurk2801@gmail.com>
2024-11-04 18:53:43 -0300

    Removed useless code and added the icon

--- src/components/DownloadBtn/index.tsx
+++ src/components/DownloadBtn/index.tsx
@@ -1,40 +1,17 @@
 import { useSelector } from "react-redux";
 
 import { RootState } from "../../store";
-import { ListItemType } from "../../models";
 
 export default function DownloadBtn() {
     const listItems = useSelector((state: RootState) => state.list.items);
 
-    //-- Reset the items ids --//
-
-    //Create a new list without the ids
-    const idLessItems = listItems.map(item => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { id, ...rest } = item;
-        const idLess = { ...rest };
-
-        return idLess;
-    })
-
-    //Create a new list with new ids
-    const newList: ListItemType[] = idLessItems.map((item, index) => {
-        const newItem: ListItemType = {
-            id: index,
-            name: item.name,
-            qtdType: item.qtdType,
-            quantity: item.quantity,
-            alertQuantity: item.alertQuantity
-        }
-
-        return newItem;
-    })
-
-    const data = JSON.stringify(newList);
+    const data = JSON.stringify(listItems);
     const blob = new Blob([data], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
 
     return (
-        <a className="btn btn-success" href={url} download='lista.json'>Baixar</a>
+        <a className="btn btn-sm btn-success" href={url} download='lista.json'>
+            <i className="bi bi-download" /> Baixar
+        </a>
     )
 }
