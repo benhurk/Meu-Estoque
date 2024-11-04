673914bc3c85c348fae3a710c08f43fd4e6dbef2
Ben Hur <benhurk2801@gmail.com>
2024-11-04 18:55:07 -0300

    Line breaks

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -45,9 +45,11 @@ export default function ListItem({id, name, qtdType, quantity, alertQuantity}: L
             </div>
             <div>
                 <button type="button" className="btn btn-sm btn-primary me-1" data-toggle="modal" data-target="#modal-form" onClick={() => setEditModal(id)}>
-                    <i className="bi bi-gear-fill"></i>
+                    <i className="bi bi-pencil-fill"></i>
                 </button>
-                <button type="button" className="btn btn-sm btn-danger" onClick={() => dispatch(removeItem(id))}><i className="bi bi-trash-fill"></i></button>
+                <button type="button" className="btn btn-sm btn-danger" onClick={() => dispatch(removeItem(id))}>
+                    <i className="bi bi-trash-fill"></i>
+                </button>
             </div>
         </li>
     )
