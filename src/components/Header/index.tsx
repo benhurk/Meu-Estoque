ca991b1793d1fd0f724b997a720f94144b996239
Ben Hur <benhurk2801@gmail.com>
2024-11-06 14:13:11 -0300

    Removed the wpp number field and the sendTo value

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { RootState } from "../../store";
@@ -12,51 +11,42 @@ export default function Header() {
     const listItems = useSelector((state: RootState) => state.list.items);
     const warnedItems = listItems.filter(item => item.quantity <= item.alertQuantity);
 
-    const [sendTo, setSendTo] = useState<string>(localStorage.getItem('send-to') || '');
-
-    useEffect(() => {
-        localStorage.setItem('send-to', sendTo);
-    }, [sendTo])
-
-
     const sendAll = () => {
         let message = '';
     
         listItems.forEach(item => {
             const abstractQuantity = abstractSwitch(item);
     
-            const itemMessage = `*• ${item.quantity <= item.alertQuantity ? '❗' : '✅'} ${item.name}:* ${item.qtdType === 'unity' ? item.quantity : abstractQuantity} ${item.qtdType === 'unity' ? 'unidades' : ''}%0a`;
+            const itemMessage = `*• ${item.quantity <= item.alertQuantity ? '⚠' : '✅'} ${item.name}:* ${item.qtdType === 'unity' ? item.quantity : abstractQuantity} ${item.qtdType === 'unity' ? 'un.' : ''}%0a`;
     
             message += itemMessage;
         })
     
-        const url = `whatsapp://send?phone=${sendTo}&text=${message}`;
+        const url = `whatsapp://send?phone=&text=${message}`;
         window.open(url);
     }
 
     const sendImportant = () => {
-        let message = '❗ *Precisa de: ❗*%0a%0a';
+        let message = '⚠   *Precisa de:*   ⚠%0a%0a';
     
         warnedItems.forEach(item => {
             const abstractQuantity = abstractSwitch(item);
     
-            const itemMessage = `*• ${item.name}* (${item.qtdType === 'unity' ? item.quantity + ' unidades' : abstractQuantity})%0a`;
+            const itemMessage = `*• ${item.name}* (${item.qtdType === 'unity' ? item.quantity + ' un.' : abstractQuantity})%0a`;
     
             message += itemMessage;
         })
     
-        const url = `whatsapp://send?phone=${sendTo}&text=${message}`;
+        const url = `whatsapp://send?phone=&text=${message}`;
         window.open(url);
     }
 
     return (
         <header className="container py-4 bg-primary">
-            <h1 className="text-center text-light">Eztoque</h1>
-            <div className="dropdown">
-                <button type="button" className="btn text-light fs-1 d-block mx-auto" data-toggle="modal" data-target="#options-menu">
+            <h1 className="text-center text-light mb-4">EZtoque</h1>
+            <button type="button" className="btn btn-light text-primary d-block mx-auto" data-toggle="modal" data-target="#options-menu">
                     <i className="bi bi-list" />
-                </button>
-            </div>
+            </button>
             <div className="modal fade" tabIndex={-1} role="dialog" id="options-menu">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
@@ -68,17 +58,11 @@ export default function Header() {
                             </div>
                         </div>
                         <div className="modal-body py-4">
-                            <div className="form-group d-flex mb-4">
-                                <label htmlFor="sendFor" className="me-2">
-                                    <i className="bi bi-whatsapp fs-4" />
-                                </label>
-                                <input type="text" id="sendFor" className="form-control" value={sendTo} placeholder={"Enviar para"} onChange={(e) => setSendTo(e.target.value)} />
-                            </div>
                             <div className="d-flex justify-content-center gap-2">
-                                <button type="button" className="btn btn-primary" disabled={listItems.length > 0 ? false : true} onClick={() => sendAll()}>
+                                <button type="button" className="btn btn-primary" disabled={listItems.length > 0 ? false : true} data-dismiss="modal" onClick={() => sendAll()}>
                                     <i className="bi bi-send-fill" /> Enviar tudo
                                 </button>
-                                <button type="button" className="btn btn-danger" disabled={warnedItems.length > 0 ? false : true} onClick={() => sendImportant()}>
+                                <button type="button" className="btn btn-danger" disabled={warnedItems.length > 0 ? false : true} data-dismiss="modal" onClick={() => sendImportant()}>
                                     <i className="bi bi-send-exclamation-fill" /> Enviar importantes
                                 </button>
                             </div>
