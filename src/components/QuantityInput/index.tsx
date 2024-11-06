d7ba075150c4eb1325adfbcb82247c8c5ca450d2
Ben Hur <benhurk2801@gmail.com>
2024-11-06 14:18:02 -0300

    Changed the minimum quantity

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -22,7 +22,7 @@ export default function QuantityInput({size = 'sm', type, value, change}: Props)
     if (type === 'abstract') {
         return (
             <select className={`w-50 form-select form-select-${size}`} value={value} onChange={change}>
-                <option value='0'>Acabando</option>
+                <option value='0'>Acabou</option>
                 <option value='1'>Pouco</option>
                 <option value='2'>Suficiente</option>
                 <option value='3'>Bastante</option>
--- src/utils/abstractSwitch.ts
+++ src/utils/abstractSwitch.ts
@@ -2,7 +2,7 @@ import { ListItemType } from "../models";
 
 export default function abstractSwitch(item: ListItemType) {
     switch (item.quantity) {
-        case 0: return 'Acabando'
+        case 0: return 'Não tem'
         case 1: return 'Pouco'
         case 2: return 'Suficiente'
         case 3: return 'Bastante'
