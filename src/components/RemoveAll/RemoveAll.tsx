e400308aeed78fb56e01a5824032ee5c42b7e963
Ben Hur <benhurk2801@gmail.com>
2024-12-27 13:24:56 -0300

    Centered the content

--- src/components/RemoveAll/RemoveAll.tsx
+++ src/components/RemoveAll/RemoveAll.tsx
@@ -13,9 +13,11 @@ export default function RemoveAll() {
                 <i className="bi bi-trash-fill" /> Apagar tudo
             </button>
             <Modal elementId="modal-remove">
-                <span className="d-block text-danger fw-bold border-bottom mb-4 pb-2">Deseja mesmo apagar tudo?</span>
-                <button type="button" className="btn btn-danger me-2" onClick={() => dispatch(clearList())} data-dismiss="modal">Apagar tudo</button>
-                <button type="button" className="btn btn-primary me-2" data-dismiss="modal">Cancelar</button>
+                <div className="text-center">
+                    <span className="d-block text-danger fw-bold border-bottom mb-4 pb-2">Deseja mesmo apagar tudo?</span>
+                    <button type="button" className="btn btn-danger me-2" onClick={() => dispatch(clearList())} data-dismiss="modal">Apagar tudo</button>
+                    <button type="button" className="btn btn-primary me-2" data-dismiss="modal">Cancelar</button>
+                </div>
             </Modal>
         </>
     )
