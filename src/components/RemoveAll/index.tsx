6e5ac90e8144aa19fa31c2c79c07b80aa26a1b33
Ben Hur <benhurk2801@gmail.com>
2024-12-28 15:15:37 -0300

    Renamed the file to index

--- src/components/RemoveAll/index.tsx
+++ src/components/RemoveAll/index.tsx
@@ -4,19 +4,24 @@ import { clearList } from "../../store/reducers/list";
 
 import Modal from "../Modal";
 
-export default function RemoveAll() {
+type Props = {
+    disabled: boolean;
+}
+
+export default function RemoveAll({disabled}: Props) {
     const dispatch = useDispatch();
 
     return (
         <>
-            <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#modal-remove">
+            <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#modal-remove" disabled={disabled}>
                 <i className="bi bi-trash-fill" /> Apagar tudo
             </button>
+
             <Modal elementId="modal-remove">
                 <div className="text-center">
-                    <span className="d-block text-danger fw-bold border-bottom mb-4 pb-2">Deseja mesmo apagar tudo?</span>
+                    <span className="d-block fs-5 text-danger fw-bold border-bottom mb-4 pb-2">Deseja mesmo apagar tudo?</span>
                     <button type="button" className="btn btn-danger me-2" onClick={() => dispatch(clearList())} data-dismiss="modal">Apagar tudo</button>
-                    <button type="button" className="btn btn-primary me-2" data-dismiss="modal">Cancelar</button>
+                    <button type="button" className="btn btn-dark me-2" data-dismiss="modal">Cancelar</button>
                 </div>
             </Modal>
         </>
