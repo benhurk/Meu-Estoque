e0af6a2cada3a9309d955ee97b7bcc861a67dabf
Ben Hur <benhurk2801@gmail.com>
2024-12-30 19:35:47 -0300

    Added the wraper class

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,23 +5,25 @@ import SendButton from "../SendButton";
 
 export default function Header() {
     return (
-        <header className="container py-5 bg-dark">
-            <h1 className="text-center text-light fw-bold mb-4">EZtoque</h1>
-            <div className="d-flex justify-content-center gap-3">
-                <button type="button" className="btn btn-sm btn-primary" data-toggle="modal" data-target="#send-menu">
-                    <i className="bi bi-send-fill" />
-                    &nbsp;Enviar
-                </button>
-                <DownloadBtn />
-                <LoadBtn />
-            </div>
-
-            <Modal elementId="send-menu">
-                <div className="d-flex justify-content-center gap-2">
-                    <SendButton sendMode="all" />
-                    <SendButton sendMode="warn" />
+        <header className="py-5 bg-dark">
+            <div className="wraper">
+                <h1 className="text-center text-light fw-bold mb-4">EZtoque</h1>
+                <div className="d-flex justify-content-center gap-3">
+                    <button type="button" className="btn btn-sm btn-primary" data-toggle="modal" data-target="#send-menu">
+                        <i className="bi bi-send-fill" />
+                        &nbsp;Enviar
+                    </button>
+                    <DownloadBtn />
+                    <LoadBtn />
                 </div>
-            </Modal>
+
+                <Modal elementId="send-menu">
+                    <div className="d-flex justify-content-center gap-2">
+                        <SendButton sendMode="all" />
+                        <SendButton sendMode="warn" />
+                    </div>
+                </Modal>
+            </div>
         </header>
     )
 }
