30e6815cca0978755db6d05ae57e2ed6d574e9e6
Ben Hur <benhurk2801@gmail.com>
2025-01-04 12:45:16 -0300

    Created a component for the form groups

---
+++ src/components/FormGroup/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+type Props = {
+    elementId: string;
+    labelText: string;
+    children: ReactNode;
+};
+
+export default function FormGroup({ elementId, labelText, children }: Props) {
+    return (
+        <div className='form-group mb-3'>
+            <label htmlFor={elementId} className='mb-1 text-sm d-block'>
+                {labelText}
+            </label>
+            {children}
+        </div>
+    );
+}
