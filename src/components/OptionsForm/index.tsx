27248effe97c711caad09dc02b767d81355173db
Ben Hur <benhurk2801@gmail.com>
2025-01-04 14:44:32 -0300

    Added the feature to save the options into a preset

--- src/components/OptionsForm/index.tsx
+++ src/components/OptionsForm/index.tsx
@@ -1,4 +1,10 @@
-import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
+import {
+    Dispatch,
+    FormEvent,
+    SetStateAction,
+    useEffect,
+    useState,
+} from 'react';
 
 type Props = {
     options: string[];
@@ -6,60 +12,144 @@ type Props = {
 };
 
 export default function OptionsForm({ options, setOptions }: Props) {
-    const [option, setOption] = useState<string>('');
+    const [mode, setMode] = useState<'add' | 'select'>('select');
+    const [savedOptions, setSavedOptions] = useState<string[][]>([
+        ['Acabou', 'Pouco', 'Suficiente', 'Bastante'],
+    ]);
+    const [newOption, setNewOption] = useState<string>('');
     const [error, setError] = useState<string>('');
+
+    useEffect(() => {
+        setOptions([]);
+    }, [mode, setOptions]);
 
     const submitOption = (e: FormEvent<HTMLButtonElement>, value: string) => {
         e.preventDefault();
 
-        if (!options.some((item) => item === value)) {
+        if (!options.some((item) => item === value) && value != '') {
             setOptions((prev) => [...prev, value]);
             if (error) setError('');
+        } else if (value === '') {
+            setError('Dê um nome para a opção');
         } else {
             setError('Essa opção já existe');
         }
 
-        setOption('');
+        setNewOption('');
     };
 
     const removeOption = (toRemove: string) => {
         setOptions((prev) => prev.filter((items) => items != toRemove));
     };
 
+    const saveOptions = (options: string[]) => {
+        if (!savedOptions.some((saved) => saved === options)) {
+            setSavedOptions((prev) => [...prev, options]);
+        }
+    };
+
     return (
-        <>
-            <div className='d-flex mb-1'>
-                <input
-                    id='item-options'
-                    className='form-control'
-                    type='text'
-                    placeholder={error ? error : `Ex: Pouco / Bastante`}
-                    value={option}
-                    onChange={(e) => setOption(e.target.value)}
-                />
-                <button
-                    type='submit'
-                    className='btn btn-dark'
-                    onClick={(e) => submitOption(e, option)}>
-                    <i className='bi bi-plus' />
-                </button>
+        <div className='mb-3'>
+            <div className='form-group mb-1'>
+                <div className='form-check form-check-inline'>
+                    <input
+                        type='radio'
+                        className='form-check-input'
+                        id='radio-select'
+                        name='mode'
+                        checked={mode === 'select'}
+                        value='select'
+                        onChange={() => setMode('select')}
+                    />
+                    <label htmlFor='radio-select' className='form-check-label'>
+                        Selecionar
+                    </label>
+                </div>
+                <div className='form-check form-check-inline'>
+                    <input
+                        type='radio'
+                        className='form-check-input'
+                        id='radio-add'
+                        name='mode'
+                        checked={mode === 'add'}
+                        value='add'
+                        onChange={() => {
+                            setMode('add');
+                            setOptions([]);
+                        }}
+                    />
+                    <label htmlFor='radio-add' className='form-check-label'>
+                        Criar
+                    </label>
+                </div>
             </div>
 
-            {options.length > 0 && (
-                <ol>
-                    {options.map((item, index) => (
-                        <li key={index}>
-                            <span className='text-primary'>{item}</span>
+            {mode === 'add' ? (
+                <>
+                    <div className='mb-1 input-group'>
+                        <input
+                            id='item-options'
+                            className='form-control'
+                            type='text'
+                            placeholder={error ? error : 'Nova opção'}
+                            value={newOption}
+                            onChange={(e) => setNewOption(e.target.value)}
+                        />
+                        <button
+                            type='submit'
+                            className='btn btn-dark'
+                            onClick={(e) => submitOption(e, newOption)}>
+                            <i className='bi bi-plus' />
+                        </button>
+                    </div>
+                    {options.length > 0 && (
+                        <>
+                            <ol className='mb-1'>
+                                {options.map((item, index) => (
+                                    <li key={index}>
+                                        <span className='text-primary'>
+                                            {item}
+                                        </span>
+                                        <button
+                                            type='button'
+                                            className='ms-1 btn btn-sm'
+                                            onClick={() => removeOption(item)}>
+                                            <i className='bi bi-x' />
+                                        </button>
+                                    </li>
+                                ))}
+                            </ol>
                             <button
                                 type='button'
-                                className='ms-1 btn btn-sm'
-                                onClick={() => removeOption(item)}>
-                                <i className='bi bi-x' />
+                                className='btn btn-sm btn-primary'
+                                onClick={() => saveOptions(options)}>
+                                Salvar opções
                             </button>
-                        </li>
+                        </>
+                    )}
+                </>
+            ) : (
+                <select
+                    className='form-select'
+                    defaultValue=''
+                    onChange={(e) =>
+                        setOptions(savedOptions[Number(e.target.value)])
+                    }>
+                    <option value='' disabled hidden>
+                        Selecione as opções
+                    </option>
+                    {savedOptions.map((option, index) => (
+                        <option key={index} value={index}>
+                            {option.join(' / ')}
+                        </option>
                     ))}
-                </ol>
+                    {savedOptions.length < 2 && (
+                        <option value='' disabled>
+                            Crie suas próprias opções no menu 'Criar'
+                        </option>
+                    )}
+                </select>
             )}
-        </>
+        </div>
     );
 }
