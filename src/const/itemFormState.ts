971e983493f642253df206cbeef1aca8aaa6fb35
Ben Hur <benhurk2801@gmail.com>
2025-01-04 12:46:30 -0300

    Created a folder to store conts

---
+++ src/const/itemFormState.ts
@@ -0,0 +1,11 @@
+import { FormState } from '../../types/FormTypes';
+
+const itemFormInitialState: FormState = {
+    name: '',
+    qtdType: 'number',
+    quantity: 0,
+    alertQuantity: 0,
+    description: '',
+};
+
+export default itemFormInitialState;
