82d0c9c7434099cdb4b435c486440f16b916516a
Ben Hur <benhurk2801@gmail.com>
2025-01-28 17:46:58 -0300

    Now Omits the selected property

--- src/types/FormTypes.ts
+++ src/types/FormTypes.ts
@@ -1,4 +1,4 @@
 import ListItemType from './ListItemTypes';
 
 export type FormMode = 'add' | 'edit';
-export type FormState = Omit<ListItemType, 'id' | 'options'>;
+export type FormState = Omit<ListItemType, 'id' | 'options' | 'selected'>;
