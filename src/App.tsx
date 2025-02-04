ba0e0a434b45e4e80fad328da066c013f3b1b301
Ben Hur <benhurk2801@gmail.com>
2025-02-04 14:19:58 -0300

    Created a component for the list and its functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,14 @@
-import { useState } from 'react';
 import './styles.css';
 
-import useListStore from './stores/listStore';
-import useFormStore from './stores/formStore';
-
 import Header from './components/Header';
-import ListItem from './components/ListItem';
-import Modal from './components/Modal';
-import ItemForm from './components/ItemForm';
-import ListButtons from './components/ListButtons';
-import PlaceholderContent from './components/PlaceholderContent';
+import List from './components/List';
 
 function App() {
-    const listItems = useListStore((state) => state.items);
-    const setFormMode = useFormStore((state) => state.setFormMode);
-
-    const [itemFormOpen, setItemFormOpen] = useState<boolean>(false);
-
     return (
         <>
             <Header />
             <main className='container'>
-                <ListButtons
-                    setFormMode={setFormMode}
-                    openFormFn={setItemFormOpen}
-                />
-                {listItems.length > 0 ? (
-                    <ul>
-                        {listItems.map((item) => (
-                            <ListItem
-                                key={item.id}
-                                item={{
-                                    id: item.id,
-                                    name: item.name,
-                                    qtdType: item.qtdType,
-                                    numberOf: item.numberOf,
-                                    quantity: item.quantity,
-                                    options: item.options,
-                                    alertQuantity: item.alertQuantity,
-                                    description: item.description,
-                                    selected: item.selected,
-                                }}
-                                setItemFormOpen={setItemFormOpen}
-                            />
-                        ))}
-                    </ul>
-                ) : (
-                    <PlaceholderContent text='Nenhum item adicionado ainda.' />
-                )}
-
-                <Modal isOpen={itemFormOpen} setIsOpen={setItemFormOpen}>
-                    <ItemForm setItemFormOpen={setItemFormOpen} />
-                </Modal>
+                <List />
             </main>
         </>
     );
---
+++ src/components/List/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import styles from './List.module.css';
+
+import useFormStore from '../../stores/formStore';
+import useListStore from '../../stores/listStore';
+
+import ListItem from '../ListItem';
+import Modal from '../Modal';
+import ItemForm from '../ItemForm';
+import PlaceholderContent from '../PlaceholderContent';
+import RemoveMultipleButton from '../RemoveMultipleButton';
+import InputWithButton from '../InputWithButton';
+
+export default function List() {
+    const [search, setSearch] = useState<string>('');
+
+    const listItems = useListStore((state) => state.items);
+    const setFormMode = useFormStore((state) => state.setFormMode);
+
+    const searchedItems = listItems.filter((item) =>
+        item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    );
+
+    const [itemFormOpen, setItemFormOpen] = useState<boolean>(false);
+
+    return (
+        <div className={styles.listContainer}>
+            <div className={styles.buttonsArea}>
+                <button
+                    type='button'
+                    className='btn btn-dark'
+                    onClick={() => {
+                        setFormMode('add');
+                        setItemFormOpen(true);
+                    }}>
+                    <i className='bi bi-plus-lg' />
+                    &nbsp;Adicionar item
+                </button>
+                <RemoveMultipleButton />
+            </div>
+            <div className={styles.searchArea}>
+                <InputWithButton
+                    buttonIconClass='bi-search'
+                    onButtonClick={(inputValue) => setSearch(inputValue)}
+                    clearAfter={false}
+                    clearBtn={() => setSearch('')}
+                    placeholderText='Pesquisar item'
+                />
+            </div>
+            {searchedItems.length > 0 ? (
+                <ul>
+                    {searchedItems.map((item) => (
+                        <ListItem
+                            key={item.id}
+                            item={{
+                                id: item.id,
+                                name: item.name,
+                                qtdType: item.qtdType,
+                                numberOf: item.numberOf,
+                                quantity: item.quantity,
+                                options: item.options,
+                                alertQuantity: item.alertQuantity,
+                                description: item.description,
+                                selected: item.selected,
+                            }}
+                            setItemFormOpen={setItemFormOpen}
+                        />
+                    ))}
+                </ul>
+            ) : (
+                <PlaceholderContent
+                    text={
+                        listItems.length === 0
+                            ? 'Nenhum item adicionado ainda.'
+                            : 'Nenhum item encontrado.'
+                    }
+                />
+            )}
+
+            <Modal isOpen={itemFormOpen} setIsOpen={setItemFormOpen}>
+                <ItemForm setItemFormOpen={setItemFormOpen} />
+            </Modal>
+        </div>
+    );
+}
