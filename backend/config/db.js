b525aa8b7351cfe2543150963e00637606b2d700
Ben Hur <benhurk2801@gmail.com>
2025-02-10 13:56:31 -0300

    Moved the initDb function to this the config/db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,3 +8,34 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 export const sql = neon(
     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 );
+
+export async function initDatabase() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password_hash VARCHAR(255) NOT NULL
+            );
+        `;
+
+        await sql`
+            CREATE TABLE IF NOT EXISTS items (
+                id SERIAL PRIMARY KEY,
+                user_id INT NOT NULL,
+                name VARCHAR(100) NOT NULL,
+                quantity_type VARCHAR(50) NOT NULL,
+                unit_of_measurement VARCHAR(50),
+                options TEXT,
+                quantity INT NOT NULL,
+                alert_quantity INT NOT NULL,
+                description TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            );
+        `;
+    } catch (error) {
+        console.log('Error initializing database.', error);
+    }
+}
