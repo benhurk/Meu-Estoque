0da284fd6590a6e77597be49fd4763dbcab6b17c
Ben Hur <benhurk2801@gmail.com>
2025-02-15 13:28:21 -0300

    Created the controller for adding items

--- backend/controllers/itemsController.js
+++ backend/controllers/itemsController.js
@@ -2,7 +2,7 @@ import { sql } from '../config/db.js';
 
 export async function getAllUserItems(req, res) {
     try {
-        const userId = req.user.id;
+        const userId = await req.user.id;
 
         const userItems = await sql`
             SELECT * FROM items WHERE user_id = ${userId};
@@ -18,4 +18,29 @@ export async function getAllUserItems(req, res) {
     }
 }
 
-export async function addNewItem(req, res) {}
+export async function addNewItem(req, res) {
+    try {
+        const userId = await req.user.id;
+        const {
+            name,
+            quantityType,
+            unitOfMeasurement,
+            quantity,
+            alertQuantity,
+            description,
+        } = await req.body;
+
+        await sql`
+            INSERT INTO items (user_id, name, quantity_type, unit_of_measurement, quantity, alert_quantity, description) 
+            VALUES (${userId}, ${name}, ${quantityType}, ${unitOfMeasurement}, ${quantity}, ${alertQuantity}, ${description});
+        `;
+
+        res.sendStatus(201);
+    } catch (error) {
+        console.log('Error while trying to addNewItem', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
