ca71a5fe0664c8b64bd4654196361256830bc585
Ben Hur <benhurk2801@gmail.com>
2025-03-08 15:21:41 -0300

    Trying to fix production

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use('/api/logs', authenticateToken, logsRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
     });
 }
 
